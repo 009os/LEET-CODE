9c903e768434e7b4659eca8d2caac638c495fba4
OMJI SHUKLA <omshukla245@gmail.com>
2023-08-24 12:31:45 +0530

    Add files via upload

---
+++ JAVA/28.java
@@ -0,0 +1,33 @@
+/*Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.*/
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int a=haystack.length();
+        int b=needle.length();
+        if(a<b)
+            return -1;
+        for(int i=0;i<=a-b;i++){
+            int j=0;
+            while(j<b && haystack.charAt(i+j)==needle.charAt(j))
+                j++;
+            if(j==b){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
